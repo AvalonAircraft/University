747fb78c735b96792dc01d855ede764e1e178f12
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 16:01:22 +0900

    Refactor role_path to use variable
    Updated role_path to use variable instead of hardcoded value.

--- stacks/iam/stepfunctions/StepFunction3_EmailWorkFLow-role/main.tf
+++ stacks/iam/stepfunctions/StepFunction3_EmailWorkFLow-role/main.tf
@@ -1,16 +1,40 @@
+# Provider
+
+provider "aws" {
+  region = var.region
+}
+
+
+# Account-agnostic helpers
+
+data "aws_caller_identity" "current" {}
+data "aws_partition"       "current" {}
+data "aws_region"          "current" {}
+
+
+# Module
+
 module "stepfn_email_workflow_role" {
-  # auf dein generisches Modul zeigen (ohne GUID im Ordnernamen)
+  # generisches Modul (ohne GUID im Ordnernamen)
   source = "../../../modules/iam/stepfunctions3_emailworkflow_role"
 
   role_name        = var.role_name
-  role_path        = "/service-role/"
+  role_path        = var.role_path
   lambda_resources = var.lambda_resources
   tags             = var.tags
 
-  # optional – wenn ich zentrale Policies schon habe, kann ich lokale Erstellung abschalten
+  # optional – wenn zentrale Policies vorhanden sind
   create_managed_policies      = var.create_managed_policies
   existing_managed_policy_arns = var.existing_managed_policy_arns
 }
 
-output "role_name" { value = module.stepfn_email_workflow_role.role_name }
-output "role_arn"  { value = module.stepfn_email_workflow_role.role_arn  }
+
+# Outputs
+
+output "role_name" {
+  value = module.stepfn_email_workflow_role.role_name
+}
+
+output "role_arn" {
+  value = module.stepfn_email_workflow_role.role_arn
+}
