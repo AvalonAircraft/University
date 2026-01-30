f50852aee9d3271447874c8b0e5e256d0fd04f9d
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 09:00:54 +0900

    Update variables.tf

--- stacks/cdn/variables.tf
+++ stacks/cdn/variables.tf
@@ -1,20 +1,32 @@
 # stacks/cdn/variables.tf
+
 variable "region" {
-  description = "Region für Authentifizierung (CloudFront ist global)"
+  description = "Region für Provider (ACM für CloudFront MUSS in us-east-1 sein; CloudFront ist global)"
   type        = string
   default     = "us-east-1"
 }
 
 # Route53-Zone, in der die DNS-Validierungs-CNAMEs erstellt werden
 variable "zone_name" {
-  description = "Öffentliche Hosted Zone, z. B. miraedrive.com"
+  description = "Öffentliche Hosted Zone, z. B. example.com (mit/ohne trailing dot)"
   type        = string
 }
 
 # Domains, für die das Zertifikat gelten soll (erstes Element = Common Name)
 variable "certificate_domains" {
-  description = "Liste der Domains für das ACM-Zertifikat (CN + SANs), z. B. [\"miraedrive.com\", \"www.miraedrive.com\"]"
+  description = "Liste der Domains für das ACM-Zertifikat (CN + SANs), z. B. [\"example.com\", \"www.example.com\"]"
   type        = list(string)
+}
+
+# Für Redirect-Function (damit nichts hardcoded ist)
+variable "apex_domain" {
+  description = "Apex Domain (Root), z.B. example.com"
+  type        = string
+}
+
+variable "www_domain" {
+  description = "WWW Domain, z.B. www.example.com"
+  type        = string
 }
 
 # Origins für die beiden Distributions
@@ -25,7 +37,6 @@ variable "redirect_custom_origin_domain" {
 }
 
 variable "web_s3_origin_domain" {
-  description = "S3-REST-Endpoint für die Web-Distribution"
+  description = "S3-REST-Endpoint für die Web-Distribution (z.B. <bucket>.s3.us-east-1.amazonaws.com)"
   type        = string
-  default     = "miraedrive-assets.s3.us-east-1.amazonaws.com"
 }
