e2e3765635c148964f628c5975bb8210862c1eb1
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 16:05:59 +0900

    Add role_path variable and improve comments
    Added a new variable 'role_path' and updated comments for clarity.

--- stacks/iam/tenantRole/variables.tf
+++ stacks/iam/tenantRole/variables.tf
@@ -1,39 +1,43 @@
-############################
 # Region
-############################
+
 variable "region" {
   type    = string
   default = "us-east-1"
 }
 
-############################
+
 # Rolle
-############################
+
 variable "role_name" {
   type    = string
   default = "TenantRole"
 }
 
-############################
+variable "role_path" {
+  type    = string
+  default = "/"
+}
+
+
 # S3 Bucket
-############################
+
 variable "bucket" {
   type    = string
   default = "miraedrive-assets"
 }
 
-############################
+
 # Optional: zusätzliche Trust-Principals
-# Wenn leer, vertraut man Root des aktuellen Accounts.
-############################
+# Wenn leer, vertraut der Stack automatisch dem Root des aktuellen Accounts.
+
 variable "trusted_principals" {
   type    = list(string)
   default = []
 }
 
-############################
+
 # Tags
-############################
+
 variable "tags" {
   type = map(string)
   default = {
