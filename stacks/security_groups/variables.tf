5e229481d94337d40b559b8ad10563ea3f38a97f
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 10:11:59 +0900

    Update variables.tf

--- stacks/security_groups/variables.tf
+++ stacks/security_groups/variables.tf
@@ -1,7 +1,22 @@
-variable "region"        { type = string, default = "us-east-1" }
-variable "vpc_id"        { type = string }          # z.B. aus module.vpc.vpc_id
-variable "private1_cidr" { type = string }          # 10.0.128.0/20
-variable "private2_cidr" { type = string }          # 10.0.144.0/20
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID (z.B. aus network/vpc stack outputs)"
+}
+
+variable "private1_cidr" {
+  type        = string
+  description = "CIDR des privaten Subnetzes 1 (z.B. 10.0.128.0/20)"
+}
+
+variable "private2_cidr" {
+  type        = string
+  description = "CIDR des privaten Subnetzes 2 (z.B. 10.0.144.0/20)"
+}
 
 variable "tags" {
   type = map(string)
@@ -9,16 +24,23 @@ variable "tags" {
     Projekt         = "MiraeDrive"
     "StartUp-Modus" = "true"
     Umgebung        = "Produktiv"
-    TenantID = ""
+    TenantID        = ""
   }
 }
 
+# OPTIONAL Override:
+# - Wenn null -> wir nutzen im main.tf automatisch private1/2 CIDRs
+# - Wenn gesetzt -> wir nutzen deine Liste
 variable "lambda_vpce_ingress_cidrs" {
-  type = list(string)
-  default = [
-    "3.218.0.0/15", "18.204.0.0/14", "52.90.0.0/15",
-    "54.152.0.0/16", "54.160.0.0/16", "54.172.0.0/15", "54.174.0.0/15",
-    "54.196.0.0/15", "54.198.0.0/16", "54.204.0.0/15", "54.208.0.0/15",
-    "54.236.0.0/15", "54.242.0.0/15", "54.210.0.0/15"
-  ]
+  type        = list(string)
+  description = "CIDRs allowed to reach Lambda VPC endpoint SG ingress. If null, defaults to the private subnet CIDRs."
+  default     = null
+  nullable    = true
+}
+
+# Optional: falls dein security-groups Modul IPv6-Handling unterstützt
+variable "enable_ipv6_ingress" {
+  type        = bool
+  description = "Enable IPv6 ingress rules where supported."
+  default     = false
 }
