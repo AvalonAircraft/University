660e47c8b1cfede6c60f7e4dc934f2bc8f51fa2d
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 15:33:45 +0900

    Refactor variables in variables.tf for clarity

--- stacks/iam/lambda5-role/variables.tf
+++ stacks/iam/lambda5-role/variables.tf
@@ -1,6 +1,36 @@
-variable "region"                { type = string, default = "us-east-1" }
-variable "role_name"             { type = string, default = "Lambda5-role-gmx3qmuy" }
-variable "use_customer_managed"  { type = bool,   default = true }
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "role_name" {
+  type    = string
+  default = "Lambda5-role-gmx3qmuy"
+}
+
+variable "role_path" {
+  type    = string
+  default = "/service-role/"
+}
+
+# PORTABLE DEFAULT: Professor-Account hat deine customer-managed Kopien sehr wahrscheinlich nicht
+variable "use_customer_managed" {
+  type    = bool
+  default = false
+}
+
+# Nur relevant wenn use_customer_managed = true
+variable "customer_basic_logs_policy_name" {
+  type        = string
+  description = "Name der kundenverwalteten Policy (Klon von AWSLambdaBasicExecutionRole)"
+  default     = "AWSLambdaBasicExecutionRole-0d5c5f13-6de7-4df6-9414-672fb66dd47c"
+}
+
+variable "customer_vpc_access_policy_name" {
+  type        = string
+  description = "Name der kundenverwalteten Policy (Klon von AWSLambdaVPCAccessExecutionRole)"
+  default     = "AWSLambdaVPCAccessExecutionRole-02e28b66-c82c-4a09-b5a5-1e3bceb961b8"
+}
 
 variable "tags" {
   type = map(string)
@@ -8,6 +38,6 @@ variable "tags" {
     Projekt         = "MiraeDrive"
     "StartUp-Modus" = "true"
     Umgebung        = "Produktiv"
-    TenantID = ""
+    TenantID        = ""
   }
 }
