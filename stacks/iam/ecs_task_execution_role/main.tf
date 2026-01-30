3de830b75bb56dbd748606b1536bfd2f0b276425
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 15:15:27 +0900

    Refactor ECS task execution role configuration
    Updated role_path to use variable instead of hardcoded value. Reformatted output blocks for consistency.

--- stacks/iam/ecs_task_execution_role/main.tf
+++ stacks/iam/ecs_task_execution_role/main.tf
@@ -6,10 +6,15 @@ module "ecs_task_execution_role" {
   source = "../../../modules/iam/ecs_task_execution_role"
 
   role_name         = var.role_name
-  role_path         = "/"
+  role_path         = var.role_path
   tags              = var.tags
   extra_policy_arns = var.extra_policy_arns
 }
 
-output "role_name" { value = module.ecs_task_execution_role.role_name }
-output "role_arn"  { value = module.ecs_task_execution_role.role_arn }
+output "role_name" {
+  value = module.ecs_task_execution_role.role_name
+}
+
+output "role_arn" {
+  value = module.ecs_task_execution_role.role_arn
+}
