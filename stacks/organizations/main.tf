3618549a392f7f8d774bb86a829c52688aaf6673
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 10:00:52 +0900

    Update main.tf

--- stacks/organizations/main.tf
+++ stacks/organizations/main.tf
@@ -1,22 +1,52 @@
-provider "aws" {
-  region = var.region
+########################################
+# stacks/organizations/variables.tf
+########################################
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
 }
 
-module "organizations" {
-  source = "../../modules/organizations"
-
-  org_feature_set   = "ALL"
-  ou_core_name      = var.ou_core_name
-  ou_tenants_name   = var.ou_tenants_name
-  tenant_id_pattern = var.tenant_id_pattern
-
-  # Beispiel
-  tenants = var.tenants
-
-  common_tags = var.tags
+# Failsafe: Organizations geht nur im AWS Organizations MANAGEMENT (payer) Account.
+# Default = false, damit Uni/Professor-Deploy nicht abbricht.
+variable "enabled" {
+  type    = bool
+  default = false
 }
 
-output "org_id"         { value = module.organizations.org_id }
-output "ou_core_id"     { value = module.organizations.ou_core_id }
-output "ou_tenants_id"  { value = module.organizations.ou_tenants_id }
-output "tenant_account_ids" { value = module.organizations.tenant_account_ids }
+variable "ou_core_name" {
+  type    = string
+  default = "Core"
+}
+
+variable "ou_tenants_name" {
+  type    = string
+  default = "Tenants"
+}
+
+variable "tenant_id_pattern" {
+  description = "Pattern for tenant ids (used for tagging / account naming rules)"
+  type        = string
+  default     = "tenant*"
+}
+
+variable "tenants" {
+  description = "List of tenants to create as accounts (ONLY works in management account)."
+  type = list(object({
+    tenant_id      = string
+    email          = string
+    account_name   = optional(string)
+    ou             = optional(string) # "core" | "tenants"
+    tags           = optional(map(string))
+  }))
+  default = []
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Project = "MiraeDrive"
+    Env     = "Prod"
+    Owner   = "Platform"
+  }
+}
