d86e16395737943dec74db9cd3767713b198f288
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 15:47:32 +0900

    Refactor AgentStepFunction role configuration

--- stacks/iam/stepfunctions/AgentStepFunction-role/main.tf
+++ stacks/iam/stepfunctions/AgentStepFunction-role/main.tf
@@ -1,9 +1,25 @@
+# Provider
+
+provider "aws" {
+  region = var.region
+}
+
+
+# Account-agnostic helpers
+
+data "aws_caller_identity" "current" {}
+data "aws_partition"       "current" {}
+data "aws_region"          "current" {}
+
+
+# Module
+
 module "stepfunctions_agent_role" {
-  # neuer generischer Modulpfad ohne GUID im Verzeichnisnamen
-  source         = "../../../modules/iam/stepfunctions_role_agent"
+  # generischer Modulpfad ohne GUID im Verzeichnisnamen
+  source = "../../../modules/iam/stepfunctions_role_agent"
 
   role_name      = var.role_name
-  role_path      = "/service-role/"
+  role_path      = var.role_path
   lambda_arns    = var.lambda_arns
   log_group_arns = var.log_group_arns
   tags           = var.tags
@@ -13,5 +29,13 @@ module "stepfunctions_agent_role" {
   existing_managed_policy_arns = var.existing_managed_policy_arns
 }
 
-output "role_name" { value = module.stepfunctions_agent_role.role_name }
-output "role_arn"  { value = module.stepfunctions_agent_role.role_arn  }
+
+# Outputs
+
+output "role_name" {
+  value = module.stepfunctions_agent_role.role_name
+}
+
+output "role_arn" {
+  value = module.stepfunctions_agent_role.role_arn
+}
