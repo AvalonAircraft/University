22790c2e8163bb38d441df7a3e50a4a4c51341d6
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 15:31:23 +0900

    Enhance IAM role variables in variables.tf
    Added new variables for role path and customer-managed policies, and updated the default value for use_customer_managed.

--- stacks/iam/lambda4-role/variables.tf
+++ stacks/iam/lambda4-role/variables.tf
@@ -1,6 +1,36 @@
-variable "region"               { type = string, default = "us-east-1" }
-variable "role_name"            { type = string, default = "Lambda4-role-0qiscamy" }
-variable "use_customer_managed" { type = bool,   default = true }
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "role_name" {
+  type    = string
+  default = "Lambda4-role-0qiscamy"
+}
+
+variable "role_path" {
+  type    = string
+  default = "/service-role/"
+}
+
+# PORTABLE DEFAULT: Professor-Account hat deine customer-managed Kopien sehr wahrscheinlich nicht
+variable "use_customer_managed" {
+  type    = bool
+  default = false
+}
+
+# Nur relevant wenn use_customer_managed = true
+variable "customer_basic_logs_policy_name" {
+  type        = string
+  description = "Name der kundenverwalteten Policy (Klon von AWSLambdaBasicExecutionRole)"
+  default     = "AWSLambdaBasicExecutionRole-4d5ec943-0a1e-455c-981e-113cc09da8a8"
+}
+
+variable "customer_vpc_access_policy_name" {
+  type        = string
+  description = "Name der kundenverwalteten Policy (Klon von AWSLambdaVPCAccessExecutionRole)"
+  default     = "AWSLambdaVPCAccessExecutionRole-abad322e-b478-4ac9-a296-28c648a6690d"
+}
 
 variable "tags" {
   type = map(string)
@@ -8,6 +38,6 @@ variable "tags" {
     Projekt         = "MiraeDrive"
     "StartUp-Modus" = "true"
     Umgebung        = "Produktiv"
-    TenantID = ""
+    TenantID        = ""
   }
 }
