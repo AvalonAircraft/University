392d5b869043177ab1780602878d6d173e85bdcd
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 16:04:52 +0900

    Refactor tenant role module configuration

--- stacks/iam/tenantRole/main.tf
+++ stacks/iam/tenantRole/main.tf
@@ -1,26 +1,38 @@
-############################
 # Provider
-############################
+
 provider "aws" {
   region = var.region
 }
 
-############################
+
+# Account-agnostic helper
+
+data "aws_partition" "current" {}
+data "aws_caller_identity" "current" {}
+
+locals {
+  # Wenn nichts übergeben wird, vertraue Root des aktuellen Accounts (portabel).
+  effective_trusted_principals = length(var.trusted_principals) > 0 ? var.trusted_principals : [
+    "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"
+  ]
+}
+
+
 # Modul: TenantRole
-############################
+
 module "tenant_role" {
   source = "../../../modules/iam/tenantRole"
 
   role_name          = var.role_name
-  role_path          = "/"
+  role_path          = var.role_path
   bucket             = var.bucket
-  trusted_principals = var.trusted_principals
+  trusted_principals = local.effective_trusted_principals
   tags               = var.tags
 }
 
-############################
+
 # Outputs
-############################
+
 output "tenant_role_name" {
   value = module.tenant_role.role_name
 }
