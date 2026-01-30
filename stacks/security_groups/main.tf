882c1b77684563dc50e7ec0751492b2be428289a
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 10:10:48 +0900

    Update main.tf

--- stacks/security_groups/main.tf
+++ stacks/security_groups/main.tf
@@ -1,24 +1,48 @@
 module "sg" {
-  source        = "../../modules/security-groups"
+  source = "../../modules/security-groups"
 
   vpc_id        = var.vpc_id
   private1_cidr = var.private1_cidr
   private2_cidr = var.private2_cidr
 
-  # Übergabe AWS-IP-Ranges für Lambda VPCE
+  # AWS-IP-Ranges / CIDRs, die Lambda VPC Endpoints erreichen dürfen
+  # (sollte im variables.tf einen Default haben, damit Prof nicht zwingend setzen muss)
   lambda_vpce_ingress_cidrs = var.lambda_vpce_ingress_cidrs
 
-  # Optional: IPv6 intern zulassen
-  # enable_ipv6_ingress = true
+  # Optional: IPv6 ingress innerhalb des VPC / endpoints zulassen
+  enable_ipv6_ingress = var.enable_ipv6_ingress
 
   tags = var.tags
 }
 
-output "sg_aurora_id"                  { value = module.sg.sg_aurora_id }
-output "sg_ecr_api_id"                 { value = module.sg.sg_ecr_api_id }
-output "sg_ecs_fargate_id"             { value = module.sg.sg_ecs_fargate_id }
-output "sg_lambda_agent_control_id"    { value = module.sg.sg_lambda_agent_control_id }
-output "sg_lambda6_to_vpce_id"         { value = module.sg.sg_lambda6_to_vpce_id }
-output "sg_secretsmanager_ep_id"       { value = module.sg.sg_secretsmanager_ep_id }
-output "sg_nlb_fargate_privatelink_id" { value = module.sg.sg_nlb_fargate_privatelink_id }
-output "sg_lambda_vpc_endpoint_id"     { value = module.sg.sg_lambda_vpc_endpoint_id }
+output "sg_aurora_id" {
+  value = module.sg.sg_aurora_id
+}
+
+output "sg_ecr_api_id" {
+  value = module.sg.sg_ecr_api_id
+}
+
+output "sg_ecs_fargate_id" {
+  value = module.sg.sg_ecs_fargate_id
+}
+
+output "sg_lambda_agent_control_id" {
+  value = module.sg.sg_lambda_agent_control_id
+}
+
+output "sg_lambda6_to_vpce_id" {
+  value = module.sg.sg_lambda6_to_vpce_id
+}
+
+output "sg_secretsmanager_ep_id" {
+  value = module.sg.sg_secretsmanager_ep_id
+}
+
+output "sg_nlb_fargate_privatelink_id" {
+  value = module.sg.sg_nlb_fargate_privatelink_id
+}
+
+output "sg_lambda_vpc_endpoint_id" {
+  value = module.sg.sg_lambda_vpc_endpoint_id
+}
