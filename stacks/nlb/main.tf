dc68bca1e148d357943133934f31d9e56bc46b9e
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 09:49:01 +0900

    Update main.tf

--- stacks/nlb/main.tf
+++ stacks/nlb/main.tf
@@ -1,39 +1,38 @@
 module "nlb" {
   source = "../../modules/nlb"
 
-  name              = "nlb-fargate"
+  name              = var.name
   vpc_id            = var.vpc_id
   subnet_ids        = [var.subnet_private1, var.subnet_private2]
   security_group_id = var.nlb_sg_id
 
-  internal        = true
-  ip_address_type = "dualstack"
+  internal        = var.internal
+  ip_address_type = var.ip_address_type
 
-  tg_name        = "fargate-nlb-targets"
-  tg_protocol    = "TCP"
-  tg_port        = 8080
+  tg_name        = var.tg_name
+  tg_protocol    = var.tg_protocol
+  tg_port        = var.service_port
   tg_target_type = "ip"
 
-  listener_protocol = "TCP"
-  listener_port     = 8080
+  listener_protocol = var.listener_protocol
+  listener_port     = var.service_port
 
-  # Health-Check (TCP)
-  hc_enabled       = true
-  hc_protocol      = "TCP"
-  hc_port          = "traffic-port"
-  hc_interval      = 10
-  hc_healthy_thr   = 3
-  hc_unhealthy_thr = 3
+  # Health-Check
+  hc_enabled       = var.hc_enabled
+  hc_protocol      = var.hc_protocol
+  hc_port          = var.hc_port
+  hc_interval      = var.hc_interval
+  hc_healthy_thr   = var.hc_healthy_thr
+  hc_unhealthy_thr = var.hc_unhealthy_thr
 
-  # Optional Hardening/Perf
-  enable_cross_zone   = true
-  deletion_protection = false
+  enable_cross_zone   = var.enable_cross_zone
+  deletion_protection = var.deletion_protection
 
   tags = var.tags
 }
 
 output "nlb_arn"          { value = module.nlb.nlb_arn }
-output "nlb_dns_name"     { value = module.nlb.nlb_dns_name }   # z.B. nlb-fargate-....elb.us-east-1.amazonaws.com
-output "nlb_zone_id"      { value = module.nlb.nlb_zone_id }     # z.B. Z26RNL4JYFTOTI
+output "nlb_dns_name"     { value = module.nlb.nlb_dns_name }
+output "nlb_zone_id"      { value = module.nlb.nlb_zone_id }
 output "target_group_arn" { value = module.nlb.target_group_arn }
 output "listener_arn"     { value = module.nlb.listener_arn }
