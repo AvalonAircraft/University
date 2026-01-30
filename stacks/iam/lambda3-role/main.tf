891e12e40cc743fd86016caa59b5a00ffbbbc013
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 15:26:33 +0900

    Refactor IAM role policies and parameters

--- stacks/iam/lambda3-role/main.tf
+++ stacks/iam/lambda3-role/main.tf
@@ -1,5 +1,10 @@
+# Provider
+
+provider "aws" {
+  region = var.region
+}
+
 data "aws_partition"       "current" {}
-data "aws_region"          "current" {}
 data "aws_caller_identity" "current" {}
 
 locals {
@@ -8,11 +13,13 @@ locals {
   # Umschalter: true = kundenverwaltete Kopien; false = AWS-Managed Standardpolicies
   use_customer_managed = var.use_customer_managed
 
+  # Customer-managed Policies (nur wenn im jeweiligen Account vorhanden)
   policy_arns_customer = [
-    "arn:${data.aws_partition.current.partition}:iam::${local.account_id}:policy/AWSLambdaBasicExecutionRole-702c2e05-7e66-486b-9ef8-4b1c12bb7d66",
-    "arn:${data.aws_partition.current.partition}:iam::${local.account_id}:policy/AWSLambdaVPCAccessExecutionRole-876f699d-a632-4f23-be58-2b0bc68ea401",
+    "arn:${data.aws_partition.current.partition}:iam::${local.account_id}:policy/${var.customer_basic_logs_policy_name}",
+    "arn:${data.aws_partition.current.partition}:iam::${local.account_id}:policy/${var.customer_vpc_access_policy_name}",
   ]
 
+  # AWS-managed Policies (immer vorhanden)
   policy_arns_aws_managed = [
     "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
     "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole",
@@ -25,7 +32,7 @@ module "iam_role_Lambda3" {
   source = "../../../modules/iam/lambda3-role"
 
   role_name   = var.role_name
-  role_path   = "/service-role/"
+  role_path   = var.role_path
   policy_arns = local.effective_policy_arns
 
   bucket_name = var.bucket_name
@@ -33,4 +40,4 @@ module "iam_role_Lambda3" {
 }
 
 output "role_name" { value = module.iam_role_Lambda3.role_name }
-output "role_arn"  { value = module.iam_role_Lambda3.role_arn  }
+output "role_arn"  { value = module.iam_role_Lambda3.role_arn }
