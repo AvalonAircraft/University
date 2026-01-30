1f445d0a4c2927c62e132307518f2b420401c5d8
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 17:14:30 +0900

    Update main.tf

--- modules/lambda/lambda5/main.tf
+++ modules/lambda/lambda5/main.tf
@@ -1,31 +1,6 @@
-############################
 # Module: Lambda5
-############################
 
-# --- Inputs ---
-variable "function_name" { type = string }                         # z.B. "Lambda5"
-variable "runtime"       { type = string, default = "python3.13" }
-variable "handler"       { type = string, default = "lambda_function.lambda_handler" }
 
-# Code (genau 1 Variante nutzen)
-variable "use_archive" { type = bool,   default = true }
-variable "source_file" { type = string, default = "" }             # z.B. ./src/lambda_function.py
-variable "filename"    { type = string, default = "" }             # alternativ fertiges ZIP
-
-# Limits
-variable "memory_size"            { type = number, default = 128 }
-variable "ephemeral_storage_size" { type = number, default = 512 }
-variable "timeout"                { type = number, default = 3 }
-variable "description"            { type = string, default = "Lambda5 function" }
-
-# ENV/Tags
-variable "env"  { type = map(string), default = { DEFAULT_CLIENTS = "", STRICT_MODE = "0" } }
-variable "tags" { type = map(string), default = {} }
-
-# Rolle (bestehend – wie in der Konsole)
-variable "existing_role_name" { type = string, default = "service-role/Lambda5-role-gmx3qmuy" }
-
-# --- Providers/Env ---
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
@@ -34,39 +9,90 @@ terraform {
   }
 }
 
+# --- Providers/Env ---
 data "aws_caller_identity" "current" {}
 data "aws_region"          "current" {}
 data "aws_partition"       "current" {}
 
-# --- Validations ---
-validation {
-  condition     = var.use_archive || length(var.filename) > 0
-  error_message = "Entweder 'use_archive=true' ODER ein fertiges ZIP in 'filename' angeben."
-}
-validation {
-  condition     = !var.use_archive || length(var.source_file) > 0
-  error_message = "Bei 'use_archive=true' muss 'source_file' gesetzt sein."
+
+# Inputs
+
+
+variable "function_name" { 
+  type        = string 
+  description = "Name der Lambda-Funktion"
 }
 
-############################
-# Log Group
-############################
+variable "runtime" { 
+  type    = string 
+  default = "python3.13" 
+}
+
+variable "handler" { 
+  type    = string 
+  default = "lambda_function.lambda_handler" 
+}
+
+# Code Management
+variable "use_archive" { 
+  type    = bool 
+  default = true 
+}
+
+variable "source_file" { 
+  type    = string 
+  default = "" 
+  validation {
+    condition     = !var.use_archive || length(var.source_file) > 0
+    error_message = "Bei 'use_archive=true' muss 'source_file' auf die Quell-Datei zeigen."
+  }
+}
+
+variable "filename" { 
+  type    = string 
+  default = "" 
+}
+
+# Limits
+variable "memory_size"            { type = number, default = 128 }
+variable "ephemeral_storage_size" { type = number, default = 512 }
+variable "timeout"                { type = number, default = 3 }
+variable "description"            { type = string, default = "Lambda5 function" }
+
+# ENV/Tags
+variable "env" { 
+  type    = map(string) 
+  default = { DEFAULT_CLIENTS = "", STRICT_MODE = "0" } 
+}
+
+variable "tags" { 
+  type    = map(string) 
+  default = {} 
+}
+
+# Rolle (Bestand aus der Konsole)
+variable "existing_role_name" { 
+  type    = string 
+  default = "service-role/Lambda5-role-gmx3qmuy" 
+}
+
+
+# Resources & Data
+
+
+# CloudWatch Log Group
 resource "aws_cloudwatch_log_group" "lg" {
   name              = "/aws/lambda/${var.function_name}"
   retention_in_days = 14
   tags              = var.tags
 }
 
-############################
-# Rolle: bestehend (aus Konsole)
-############################
+# Referenz auf bestehende IAM Rolle
 data "aws_iam_role" "existing" {
   name = var.existing_role_name
 }
 
-############################
-# Code-Paket
-############################
+# Paketierung
 data "archive_file" "pkg" {
   count       = var.use_archive ? 1 : 0
   type        = "zip"
@@ -75,13 +101,14 @@ data "archive_file" "pkg" {
 }
 
 locals {
-  code_filename    = var.use_archive ? data.archive_file.pkg[0].output_path         : var.filename
+  code_filename    = var.use_archive ? data.archive_file.pkg[0].output_path     : var.filename
   code_source_hash = var.use_archive ? data.archive_file.pkg[0].output_base64sha256 : filebase64sha256(var.filename)
 }
 
-############################
+
 # Lambda Function
-############################
+
+
 resource "aws_lambda_function" "fn" {
   function_name = var.function_name
   role          = data.aws_iam_role.existing.arn
@@ -96,17 +123,23 @@ resource "aws_lambda_function" "fn" {
   timeout       = var.timeout
   architectures = ["x86_64"]
 
-  ephemeral_storage { size = var.ephemeral_storage_size }
+  ephemeral_storage { 
+    size = var.ephemeral_storage_size 
+  }
 
-  environment { variables = var.env }
+  environment { 
+    variables = var.env 
+  }
 
+  # Abhängigkeit zur Log-Gruppe für sauberes Provisioning
   depends_on = [aws_cloudwatch_log_group.lg]
 
   tags = var.tags
 }
 
-############################
+
 # Outputs
-############################
+
+
 output "lambda_function_arn" { value = aws_lambda_function.fn.arn }
 output "lambda_role_arn"     { value = data.aws_iam_role.existing.arn }
