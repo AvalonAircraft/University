2191b094d7b4b18be6e9ddadd58778958e60243c
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 20:19:39 +0900

    Update main.tf

--- modules/iam/lambda4-role/main.tf
+++ modules/iam/lambda4-role/main.tf
@@ -1,25 +1,49 @@
-############################
-# Inputs
-############################
-variable "role_name"   { type = string }
-variable "role_path"   { type = string,  default = "/service-role/" }
-variable "policy_arns" { type = list(string), default = [] }
-variable "tags"        { type = map(string),   default = {} }
+# Module: IAM-Lambda-Universal
 
-# Guard, damit ich keine leeren Namen übergebe
-variable "role_name" {
-  type = string
 
-  validation {
-    condition     = length(var.role_name) > 0
-    error_message = "role_name darf nicht leer sein."
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
   }
 }
 
 
-############################
-# Trust policy (Lambda)
-############################
+# Inputs
+
+variable "role_name" {
+  type        = string
+  description = "Name der IAM Rolle"
+
+  validation {
+    # trim entfernt Leerzeichen, um sicherzustellen, dass der Name echten Inhalt hat
+    condition     = length(trim(var.role_name, " ")) > 0
+    error_message = "Der role_name darf nicht leer sein oder nur aus Leerzeichen bestehen."
+  }
+}
+
+variable "role_path" { 
+  type    = string 
+  default = "/service-role/" 
+}
+
+variable "policy_arns" { 
+  type        = list(string) 
+  default     = [] 
+  description = "Liste von Managed Policy ARNs (z.B. AWSLambdaBasicExecutionRole)"
+}
+
+variable "tags" { 
+  type    = map(string) 
+  default = {} 
+}
+
+
+# Trust Policy (Lambda)
+
 data "aws_iam_policy_document" "trust_lambda" {
   statement {
     effect = "Allow"
@@ -31,9 +55,9 @@ data "aws_iam_policy_document" "trust_lambda" {
   }
 }
 
-############################
+
 # IAM Role
-############################
+
 resource "aws_iam_role" "this" {
   name               = var.role_name
   path               = var.role_path
@@ -41,17 +65,17 @@ resource "aws_iam_role" "this" {
   tags               = var.tags
 }
 
-############################
-# Attach managed policies
-############################
+
+# Attach Managed Policies
+
 resource "aws_iam_role_policy_attachment" "attached" {
   for_each   = toset(var.policy_arns)
   role       = aws_iam_role.this.name
   policy_arn = each.value
 }
 
-############################
+
 # Outputs
-############################
+
 output "role_name" { value = aws_iam_role.this.name }
-output "role_arn"  { value = aws_iam_role.this.arn  }
+output "role_arn"  { value = aws_iam_role.this.arn }
