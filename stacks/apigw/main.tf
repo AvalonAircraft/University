75ca453a83f17af8fd0b4a13928a25e8e2901c3f
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 13:58:50 +0900

    Update Lambda ARNs in API Gateway module

--- stacks/apigw/main.tf
+++ stacks/apigw/main.tf
@@ -1,12 +1,39 @@
-# stacks/apigw/main.tf
-module "apigw" {
-  source            = "../../modules/apigw_rest"
-  api_name          = var.api_name
-  api_description   = var.api_description
-  lambda_arn_aurora = var.lambda_arn_aurora
-  lambda_arn_agent  = var.lambda_arn_agent
-  s3_bucket_name    = var.s3_bucket_name
+provider "aws" {
+  region = var.region
 }
 
-output "api_id"     { value = module.apigw.api_id }
-output "invoke_url" { value = module.apigw.invoke_url }
+
+# Resolve Lambda ARNs by function name
+
+data "aws_lambda_function" "aurora" {
+  function_name = var.lambda_aurora_function_name
+}
+
+data "aws_lambda_function" "agent" {
+  function_name = var.lambda_agent_function_name
+}
+
+
+# API Gateway Module
+
+module "apigw" {
+  source          = "../../modules/apigw_rest"
+  api_name        = var.api_name
+  api_description = var.api_description
+
+  lambda_arn_aurora = data.aws_lambda_function.aurora.arn
+  lambda_arn_agent  = data.aws_lambda_function.agent.arn
+
+  s3_bucket_name = var.s3_bucket_name
+}
+
+
+# Outputs
+
+output "api_id" {
+  value = module.apigw.api_id
+}
+
+output "invoke_url" {
+  value = module.apigw.invoke_url
+}
