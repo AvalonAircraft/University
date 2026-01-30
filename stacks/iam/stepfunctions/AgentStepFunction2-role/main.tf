4e4dce347c36a0c0d785525bf1c47d70dc360482
AvalonAircraft <ceo@avalonaircraft.com>
2026-01-30 15:56:57 +0900

    Refactor role path to use variable in main.tf

--- stacks/iam/stepfunctions/AgentStepFunction2-role/main.tf
+++ stacks/iam/stepfunctions/AgentStepFunction2-role/main.tf
@@ -1,9 +1,25 @@
+# Provider
+
+provider "aws" {
+  region = var.region
+}
+
+
+# Account-agnostic helpers
+
+data "aws_caller_identity" "current" {}
+data "aws_partition"       "current" {}
+data "aws_region"          "current" {}
+
+
+# Module
+
 module "stepfunctions_agent2_role" {
-  # ACHTUNG: neuer, generischer Modulpfad ohne GUID im Ordnernamen
+  # neuer, generischer Modulpfad ohne GUID im Ordnernamen
   source = "../../../modules/iam/stepfunctions_role_agent2"
 
   role_name        = var.role_name
-  role_path        = "/service-role/"
+  role_path        = var.role_path
   lambda_resources = var.lambda_resources
   log_group_arns   = var.log_group_arns
   tags             = var.tags
@@ -13,5 +29,13 @@ module "stepfunctions_agent2_role" {
   existing_managed_policy_arns = var.existing_managed_policy_arns
 }
 
-output "role_name" { value = module.stepfunctions_agent2_role.role_name }
-output "role_arn"  { value = module.stepfunctions_agent2_role.role_arn  }
+
+# Outputs
+
+output "role_name" {
+  value = module.stepfunctions_agent2_role.role_name
+}
+
+output "role_arn" {
+  value = module.stepfunctions_agent2_role.role_arn
+}
